7836d3f756a1deec24836cf3bb56f4a15c60a362
1rua <2752260457@qq.com>
2026-05-12 01:18:21 +0800

    练习2-3 输出倒三角图案 分数 5 作者 陈建海 单位 浙江大学
    本题要求编写程序，输出指定的由“*”组成的倒三角图案。
    输入格式:
    
    本题目没有输入。
    输出格式:
    
    按照下列格式输出由“*”组成的倒三角图案。
    * * * *
     * * *
      * *
       *
    代码长度限制
    16 KB
    时间限制
    400 ms
    内存限制
    64 MB
    栈限制
    8192 KB

--- 编程题/1-2/输出倒三角图案.c
+++ 编程题/1-2/输出倒三角图案.c
@@ -2,12 +2,15 @@
 int main(void)
 {
     int j, i;
-    for(i = 0; i<5; i++){
+    for(i = 0; i<4; i++){
         for(j = 0; j<i; j++){
         printf(" ");
     }
         for(j = 0; j<4-i; j++){
-        printf(" *");
+            if (j > 0){
+                 printf(" ");
+            }
+        printf("*");
     }
     printf("\n");
 }
