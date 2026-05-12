6996f70bcb496a61d3934d771c5d1b545ccfb0c3
1rua <2752260457@qq.com>
2026-05-12 21:05:50 +0800

    计算物体自由下落的距离

--- 编程题/4计算物体自由下落的距离/计算物体自由下落的距离.c
+++ 编程题/4计算物体自由下落的距离/计算物体自由下落的距离.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
+#include <math.h>
 int main(void){
-    int a = 100
-    
+    int i = 3;
+    double height;
+    height = 10 * pow(i,2) / 2;
+    printf("height = %.2f\n",height);
+    return 0;
 }
